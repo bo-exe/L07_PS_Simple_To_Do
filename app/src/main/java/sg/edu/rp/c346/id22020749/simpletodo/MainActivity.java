6595d33d6d16df5fb20eccbee5bb2852c733b127
boyang <22020749@myrp.edu.sg>
2023-06-01 14:40:06 +0800

    up to enhancement 1

--- app/src/main/java/sg/edu/rp/c346/id22020749/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020749/simpletodo/MainActivity.java
@@ -3,12 +3,105 @@ package sg.edu.rp.c346.id22020749.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etToDo;
+    Button btnAdd;
+    Button btnDelete;
+    Button btnClear;
+    ListView lvToDo;
+    Spinner spinAddRemove;
+
+    ArrayList<String> alToDo;
+    ArrayAdapter<String> aaToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etToDo = findViewById(R.id.editText);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnDelete = findViewById(R.id.buttonDelete);
+        btnClear = findViewById(R.id.buttonClear);
+        lvToDo = findViewById(R.id.ToDoList);
+        spinAddRemove = findViewById(R.id.spinner);
+
+        alToDo = new ArrayList<>();
+
+        aaToDo = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, alToDo);
+
+        lvToDo.setAdapter(aaToDo);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnAdd.isEnabled()) {
+                    String newToDo = etToDo.getText().toString();
+                    alToDo.add(newToDo);
+                    aaToDo.notifyDataSetChanged();
+                    etToDo.setText("");
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnDelete.isEnabled()) {
+                    String indexStr = etToDo.getText().toString();
+                    if (!indexStr.isEmpty()) {
+                        int pos = Integer.parseInt(indexStr);
+                        if (pos >= 0 && pos < alToDo.size()) {
+                            alToDo.remove(pos);
+                            aaToDo.notifyDataSetChanged();
+                            etToDo.setText("");
+                        }
+                    }
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alToDo.clear();
+                aaToDo.notifyDataSetChanged();
+            }
+        });
+
+        spinAddRemove.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        etToDo.setHint("Type in a new task here");
+                        btnAdd.setEnabled(true);
+                        btnDelete.setEnabled(false);
+                    case 1:
+                        etToDo.setHint("Type in the index of the task to be removed");
+                        btnAdd.setEnabled(false);
+                        btnDelete.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
