00bf0d7d0d66e84a2e7b058a10e92d362f4df280
boyang <22020749@myrp.edu.sg>
2023-06-01 15:20:54 +0800

    https://www.youtub.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs

--- app/src/main/java/sg/edu/rp/c346/id22020749/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020749/simpletodo/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id22020749.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -61,14 +63,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (btnDelete.isEnabled()) {
-                    String indexStr = etToDo.getText().toString();
-                    if (!indexStr.isEmpty()) {
-                        int pos = Integer.parseInt(indexStr);
-                        if (pos >= 0 && pos < alToDo.size()) {
-                            alToDo.remove(pos);
+                    String index1 = etToDo.getText().toString();
+                    if (!index1.isEmpty()) {
+                        int index = Integer.parseInt(index1);
+                        if (index >= 0 && index < alToDo.size()) {
+                            alToDo.remove(index);
                             aaToDo.notifyDataSetChanged();
                             etToDo.setText("");
+                        } else {
+                            Toast.makeText(MainActivity.this, "Wrong index number", Toast.LENGTH_SHORT).show();
                         }
+                    } else {
+                        Toast.makeText(MainActivity.this, "You don't have any task to remove", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -85,15 +91,19 @@ public class MainActivity extends AppCompatActivity {
         spinAddRemove.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Boyang","onItemSelected" + position);
+
                 switch (position) {
                     case 0:
                         etToDo.setHint("Type in a new task here");
                         btnAdd.setEnabled(true);
                         btnDelete.setEnabled(false);
+                        break;
                     case 1:
                         etToDo.setHint("Type in the index of the task to be removed");
                         btnAdd.setEnabled(false);
                         btnDelete.setEnabled(true);
+                        break;
                 }
             }
 
